d7d764025d5f4b90db9c0ae5d5e0ed3fbc66dae2
Jon Reeve <reeve.jonathan@gmail.com>
2017-10-04 09:34:29 -0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World - Openworld!!!!"))
+	w.Write([]byte("Hello Open World - CERN!!!!"))
 }
 
 func main() {
